58ecc406981b4bb4f026a0067a1766a55d494e9c
BISOON <bison.b7@gmail.com>
2017-04-04 20:29:18 +0300

    Add files via upload

---
+++ BO II EBOOT Builder/BO II EBOOT Builder/frmMain.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace BO_II_EBOOT_Builder
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.thirteenForm1 = new New_Style_1.Class1.ThirteenForm();
+            this.buildBtn = new New_Style_1.Class1.ThirteenButton();
+            this.thirteenControlBox1 = new New_Style_1.Class1.ThirteenControlBox();
+            this.vsatCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.uavCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.whCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.laserCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.rbCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.saCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.nrCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.antifreezeCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.apCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.fpsCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.fpsBox = new New_Style_1.Class1.ThirteenTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startCombo = new New_Style_1.Class1.ThirteenComboBox();
+            this.antiBanCh = new New_Style_1.Class1.ThirteenCheckBox();
+            this.thirteenForm1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // thirteenForm1
+            // 
+            this.thirteenForm1.AccentColor = System.Drawing.Color.OrangeRed;
+            this.thirteenForm1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenForm1.ColorScheme = New_Style_1.Class1.ThirteenForm.ColorSchemes.Dark;
+            this.thirteenForm1.Controls.Add(this.antiBanCh);
+            this.thirteenForm1.Controls.Add(this.startCombo);
+            this.thirteenForm1.Controls.Add(this.label1);
+            this.thirteenForm1.Controls.Add(this.fpsBox);
+            this.thirteenForm1.Controls.Add(this.fpsCh);
+            this.thirteenForm1.Controls.Add(this.apCh);
+            this.thirteenForm1.Controls.Add(this.antifreezeCh);
+            this.thirteenForm1.Controls.Add(this.nrCh);
+            this.thirteenForm1.Controls.Add(this.saCh);
+            this.thirteenForm1.Controls.Add(this.rbCh);
+            this.thirteenForm1.Controls.Add(this.laserCh);
+            this.thirteenForm1.Controls.Add(this.whCh);
+            this.thirteenForm1.Controls.Add(this.uavCh);
+            this.thirteenForm1.Controls.Add(this.vsatCh);
+            this.thirteenForm1.Controls.Add(this.buildBtn);
+            this.thirteenForm1.Controls.Add(this.thirteenControlBox1);
+            this.thirteenForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.thirteenForm1.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.thirteenForm1.ForeColor = System.Drawing.Color.White;
+            this.thirteenForm1.Location = new System.Drawing.Point(0, 0);
+            this.thirteenForm1.Name = "thirteenForm1";
+            this.thirteenForm1.Size = new System.Drawing.Size(343, 408);
+            this.thirteenForm1.TabIndex = 0;
+            this.thirteenForm1.Text = "BO II EBOOT Builder";
+            // 
+            // buildBtn
+            // 
+            this.buildBtn.AccentColor = System.Drawing.Color.OrangeRed;
+            this.buildBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.buildBtn.ColorScheme = New_Style_1.Class1.ThirteenButton.ColorSchemes.Dark;
+            this.buildBtn.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.buildBtn.ForeColor = System.Drawing.Color.White;
+            this.buildBtn.Location = new System.Drawing.Point(47, 373);
+            this.buildBtn.Name = "buildBtn";
+            this.buildBtn.Size = new System.Drawing.Size(239, 23);
+            this.buildBtn.TabIndex = 1;
+            this.buildBtn.Text = "Build";
+            this.buildBtn.UseVisualStyleBackColor = false;
+            this.buildBtn.Click += new System.EventHandler(this.buildBtn_Click);
+            // 
+            // thirteenControlBox1
+            // 
+            this.thirteenControlBox1.AccentColor = System.Drawing.Color.OrangeRed;
+            this.thirteenControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.thirteenControlBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenControlBox1.ColorScheme = New_Style_1.Class1.ThirteenControlBox.ColorSchemes.Dark;
+            this.thirteenControlBox1.ForeColor = System.Drawing.Color.White;
+            this.thirteenControlBox1.Location = new System.Drawing.Point(243, 0);
+            this.thirteenControlBox1.Name = "thirteenControlBox1";
+            this.thirteenControlBox1.Size = new System.Drawing.Size(100, 25);
+            this.thirteenControlBox1.TabIndex = 0;
+            this.thirteenControlBox1.Text = "thirteenControlBox1";
+            // 
+            // vsatCh
+            // 
+            this.vsatCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.vsatCh.Checked = false;
+            this.vsatCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.vsatCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.vsatCh.ForeColor = System.Drawing.Color.White;
+            this.vsatCh.Location = new System.Drawing.Point(47, 52);
+            this.vsatCh.Name = "vsatCh";
+            this.vsatCh.Size = new System.Drawing.Size(58, 17);
+            this.vsatCh.TabIndex = 2;
+            this.vsatCh.Text = "VSAT";
+            // 
+            // uavCh
+            // 
+            this.uavCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.uavCh.Checked = false;
+            this.uavCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.uavCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.uavCh.ForeColor = System.Drawing.Color.White;
+            this.uavCh.Location = new System.Drawing.Point(233, 52);
+            this.uavCh.Name = "uavCh";
+            this.uavCh.Size = new System.Drawing.Size(53, 17);
+            this.uavCh.TabIndex = 3;
+            this.uavCh.Text = "UAV";
+            // 
+            // whCh
+            // 
+            this.whCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.whCh.Checked = false;
+            this.whCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.whCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.whCh.ForeColor = System.Drawing.Color.White;
+            this.whCh.Location = new System.Drawing.Point(231, 184);
+            this.whCh.Name = "whCh";
+            this.whCh.Size = new System.Drawing.Size(81, 17);
+            this.whCh.TabIndex = 4;
+            this.whCh.Text = "WallHack";
+            // 
+            // laserCh
+            // 
+            this.laserCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.laserCh.Checked = false;
+            this.laserCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.laserCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.laserCh.ForeColor = System.Drawing.Color.White;
+            this.laserCh.Location = new System.Drawing.Point(231, 96);
+            this.laserCh.Name = "laserCh";
+            this.laserCh.Size = new System.Drawing.Size(57, 17);
+            this.laserCh.TabIndex = 5;
+            this.laserCh.Text = "Laser";
+            // 
+            // rbCh
+            // 
+            this.rbCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.rbCh.Checked = false;
+            this.rbCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.rbCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.rbCh.ForeColor = System.Drawing.Color.White;
+            this.rbCh.Location = new System.Drawing.Point(47, 96);
+            this.rbCh.Name = "rbCh";
+            this.rbCh.Size = new System.Drawing.Size(71, 17);
+            this.rbCh.TabIndex = 6;
+            this.rbCh.Text = "RedBox";
+            // 
+            // saCh
+            // 
+            this.saCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.saCh.Checked = false;
+            this.saCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.saCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.saCh.ForeColor = System.Drawing.Color.White;
+            this.saCh.Location = new System.Drawing.Point(231, 140);
+            this.saCh.Name = "saCh";
+            this.saCh.Size = new System.Drawing.Size(89, 17);
+            this.saCh.TabIndex = 7;
+            this.saCh.Text = "SteadyAim";
+            // 
+            // nrCh
+            // 
+            this.nrCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.nrCh.Checked = false;
+            this.nrCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.nrCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.nrCh.ForeColor = System.Drawing.Color.White;
+            this.nrCh.Location = new System.Drawing.Point(47, 140);
+            this.nrCh.Name = "nrCh";
+            this.nrCh.Size = new System.Drawing.Size(79, 17);
+            this.nrCh.TabIndex = 8;
+            this.nrCh.Text = "NoRecoil";
+            // 
+            // antifreezeCh
+            // 
+            this.antifreezeCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.antifreezeCh.Checked = false;
+            this.antifreezeCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.antifreezeCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.antifreezeCh.ForeColor = System.Drawing.Color.White;
+            this.antifreezeCh.Location = new System.Drawing.Point(47, 184);
+            this.antifreezeCh.Name = "antifreezeCh";
+            this.antifreezeCh.Size = new System.Drawing.Size(88, 17);
+            this.antifreezeCh.TabIndex = 9;
+            this.antifreezeCh.Text = "AntiFreeze";
+            // 
+            // apCh
+            // 
+            this.apCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.apCh.Checked = false;
+            this.apCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.apCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.apCh.ForeColor = System.Drawing.Color.White;
+            this.apCh.Location = new System.Drawing.Point(47, 228);
+            this.apCh.Name = "apCh";
+            this.apCh.Size = new System.Drawing.Size(107, 17);
+            this.apCh.TabIndex = 10;
+            this.apCh.Text = "AntiProbation";
+            // 
+            // fpsCh
+            // 
+            this.fpsCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.fpsCh.Checked = false;
+            this.fpsCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.fpsCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.fpsCh.ForeColor = System.Drawing.Color.White;
+            this.fpsCh.Location = new System.Drawing.Point(231, 228);
+            this.fpsCh.Name = "fpsCh";
+            this.fpsCh.Size = new System.Drawing.Size(48, 17);
+            this.fpsCh.TabIndex = 11;
+            this.fpsCh.Text = "FPS";
+            this.fpsCh.CheckedChanged += new New_Style_1.Class1.ThirteenCheckBox.CheckedChangedEventHandler(this.fpsCh_CheckedChanged);
+            // 
+            // fpsBox
+            // 
+            this.fpsBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.fpsBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fpsBox.ColorScheme = New_Style_1.Class1.ThirteenTextBox.ColorSchemes.Dark;
+            this.fpsBox.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.fpsBox.ForeColor = System.Drawing.Color.White;
+            this.fpsBox.Location = new System.Drawing.Point(29, 286);
+            this.fpsBox.Name = "fpsBox";
+            this.fpsBox.Size = new System.Drawing.Size(291, 25);
+            this.fpsBox.TabIndex = 12;
+            this.fpsBox.Text = "FPS Text";
+            this.fpsBox.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Start On";
+            // 
+            // startCombo
+            // 
+            this.startCombo.AccentColor = System.Drawing.Color.OrangeRed;
+            this.startCombo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.startCombo.ColorScheme = New_Style_1.Class1.ThirteenComboBox.ColorSchemes.Dark;
+            this.startCombo.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.startCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.startCombo.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.startCombo.ForeColor = System.Drawing.Color.White;
+            this.startCombo.FormattingEnabled = true;
+            this.startCombo.Items.AddRange(new object[] {
+            "MultiPlayer",
+            "Zombie"});
+            this.startCombo.Location = new System.Drawing.Point(96, 327);
+            this.startCombo.Name = "startCombo";
+            this.startCombo.Size = new System.Drawing.Size(192, 26);
+            this.startCombo.TabIndex = 14;
+            // 
+            // antiBanCh
+            // 
+            this.antiBanCh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.antiBanCh.Checked = false;
+            this.antiBanCh.ColorScheme = New_Style_1.Class1.ThirteenCheckBox.ColorSchemes.Dark;
+            this.antiBanCh.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Bold);
+            this.antiBanCh.ForeColor = System.Drawing.Color.White;
+            this.antiBanCh.Location = new System.Drawing.Point(137, 263);
+            this.antiBanCh.Name = "antiBanCh";
+            this.antiBanCh.Size = new System.Drawing.Size(72, 17);
+            this.antiBanCh.TabIndex = 15;
+            this.antiBanCh.Text = "AntiBan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 408);
+            this.Controls.Add(this.thirteenForm1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "EB By BISOON";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.thirteenForm1.ResumeLayout(false);
+            this.thirteenForm1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private New_Style_1.Class1.ThirteenForm thirteenForm1;
+        private New_Style_1.Class1.ThirteenButton buildBtn;
+        private New_Style_1.Class1.ThirteenControlBox thirteenControlBox1;
+        private New_Style_1.Class1.ThirteenCheckBox vsatCh;
+        private New_Style_1.Class1.ThirteenCheckBox uavCh;
+        private New_Style_1.Class1.ThirteenCheckBox whCh;
+        private New_Style_1.Class1.ThirteenCheckBox rbCh;
+        private New_Style_1.Class1.ThirteenCheckBox laserCh;
+        private New_Style_1.Class1.ThirteenCheckBox saCh;
+        private New_Style_1.Class1.ThirteenCheckBox antifreezeCh;
+        private New_Style_1.Class1.ThirteenCheckBox nrCh;
+        private New_Style_1.Class1.ThirteenCheckBox apCh;
+        private New_Style_1.Class1.ThirteenCheckBox fpsCh;
+        private New_Style_1.Class1.ThirteenCheckBox antiBanCh;
+        private New_Style_1.Class1.ThirteenComboBox startCombo;
+        private System.Windows.Forms.Label label1;
+        private New_Style_1.Class1.ThirteenTextBox fpsBox;
+    }
+}
+
---
+++ BO II EBOOT Builder/BO II EBOOT Builder/frmMain.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace BO_II_EBOOT_Builder
+{
+    public partial class frmMain : Form
+    {
+        string tmp = Path.GetTempPath() + "BO2EB";
+        const string make = "make.exe";
+        const string bo2 = "BO2.elf";
+        BinaryWriter bn;
+        SaveFileDialog sv;
+        
+         struct Offset
+        {
+            public static uint NoRecoil = 0xE9E54,
+                PrbationByPass = 0x52FC6C,
+                VSAT = 0x23C60,
+                WallHack = 0x734D0,
+                Laser = 0xDF68F, // On 01
+                SteadyAim = 0x5E0A23, //On 00 off 02
+                RedBox1 = 0x683E0,
+                RedBox2 = 0x68604,
+                AutoStartOn = 0x4525F0, // 40 ZM // 41 MP
+                UAV = 0x33CB7, // On 01 off 00
+                EndProbation = 0x96012C, // 0x00
+                FPS2 = 0x27FEC, // On 60 00 00 00
+                FPS1 = 0x8D3590,// 0x94, 50, 0, 0, 0, 0, 0, 32, 32, 32, 32, 32, 32 
+                AntiFreeze1 = 0x66B824,
+                FPSText = 0x8D3290,
+                AntiFreeze2 = 0x66B798;
+            public static uint WallHack2 = 0x734D0;
+
+            public struct Bytes
+            {
+                public static byte[] NOPE = { 0x60, 0x00, 0x00, 0x00 };
+            }
+
+        }
+        void ExecCmd(string file, string arg)
+        {
+            Process pro = new Process();
+            pro.StartInfo.FileName = file;
+            pro.StartInfo.WorkingDirectory = tmp;
+            pro.StartInfo.Arguments = arg;
+            pro.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            pro.StartInfo.CreateNoWindow = true;
+            pro.StartInfo.UseShellExecute = false;
+            pro.Start();
+            pro.WaitForExit();
+        }
+        void PatchOffset(uint Offset, byte[] value)
+        {
+            bn.Seek((int)Offset, SeekOrigin.Begin);
+            bn.Write(value);
+            bn.Flush();
+            Application.DoEvents();
+        }
+        void PatchOffset(uint Offset, byte value)
+        {
+            bn.Seek((int)Offset, SeekOrigin.Begin);
+            bn.Write(value);
+            bn.Flush();
+            Application.DoEvents();
+        }
+        void LoadFiles(string path,byte[] fileBytes)
+        {
+            File.WriteAllBytes(path, fileBytes);
+        }
+        public frmMain()
+        {      
+            InitializeComponent();           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(tmp))
+            {
+                Directory.CreateDirectory(tmp);
+            }
+            tmp += "\\";
+            if (!File.Exists(tmp + make))
+            {
+                LoadFiles(tmp + make, Properties.Resources.make);
+            }
+            if (!File.Exists(tmp + bo2))
+            {
+                LoadFiles(tmp + bo2, Properties.Resources.bo2);
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Directory.Delete(tmp, true);
+        }
+
+        private void fpsCh_CheckedChanged(object sender)
+        {
+            if (fpsCh.Checked)
+                fpsBox.Visible = true;
+            else
+                fpsBox.Visible = false;
+        }
+        private void buildBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bn = new BinaryWriter(File.Open(tmp + bo2, FileMode.Open, FileAccess.Write));
+                if (vsatCh.Checked)
+                {
+                    PatchOffset(Offset.VSAT, Offset.Bytes.NOPE);
+                }
+                if (uavCh.Checked)
+                {
+                    PatchOffset(Offset.UAV, new byte[] { 0x01 });
+                }
+                if (saCh.Checked)
+                {
+                    PatchOffset(Offset.SteadyAim, new byte[] { 0x00 });
+                }
+                if (nrCh.Checked)
+                {
+                    PatchOffset(Offset.NoRecoil, Offset.Bytes.NOPE);
+                }
+                if (rbCh.Checked)
+                {
+                    PatchOffset(Offset.RedBox1, new byte[] { 0x38, 0x60, 0x00, 0x01 });
+                    PatchOffset(Offset.RedBox2, Offset.Bytes.NOPE);
+                }
+                if (laserCh.Checked)
+                {
+                    PatchOffset(Offset.Laser, new byte[] { 0x01 });
+                }
+                if (whCh.Checked)
+                {
+                    PatchOffset(Offset.WallHack2, new byte[] { 0x38, 0xC0, 0xFF, 0xFF });
+                }
+                if (antifreezeCh.Checked)
+                {
+                    PatchOffset(Offset.AntiFreeze1, Offset.Bytes.NOPE);
+                    PatchOffset(Offset.AntiFreeze2, Offset.Bytes.NOPE);
+                }
+                if (apCh.Checked)
+                {
+                    PatchOffset(Offset.PrbationByPass, new byte[] { 0x41, 0x80 });
+                    PatchOffset(Offset.EndProbation, new byte[] { 0x00, 0x00 });
+                }
+                if (antiBanCh.Checked)
+                {
+                    byte[] anti = new byte[4];
+                    anti[0] = 0x60;
+                    PatchOffset(0x4b8310, new byte[] { 0x40, 0x00 });
+                    PatchOffset(0x50a38f, new byte[] { 0x99 });
+                    PatchOffset(0x50ba74, anti);
+                    PatchOffset(0x547dd4, anti);
+                    PatchOffset(0x548148, anti);
+                    PatchOffset(0x50b618, new byte[] { 0x48, 0x00 });
+                    PatchOffset(0x50a3bc, new byte[] { 0x48, 0x80 });
+                    PatchOffset(0x5300e8, anti);
+                    PatchOffset(0x5300f4, anti);
+                }
+                if (fpsCh.Checked)
+                {
+                    PatchOffset(Offset.FPS1, new byte[] { 0x94, 50, 0, 0, 0, 0, 0, 32, 32, 32, 32, 32, 32 });
+                    PatchOffset(Offset.FPS2, Offset.Bytes.NOPE);
+                    byte[] text = Encoding.UTF8.GetBytes(fpsBox.Text + "\0");
+                    PatchOffset(Offset.FPSText, text);
+                }
+                byte[] startGame = new byte[] { 0x41, 0x40 };
+                if (startCombo.SelectedIndex < 0)
+                {
+                    startCombo.SelectedIndex = 0;
+                }
+                PatchOffset(Offset.AutoStartOn, startGame[startCombo.SelectedIndex]);
+                bn.Close();
+                sv = new SaveFileDialog();
+                sv.Filter = "BIN|*.BIN";
+                if (sv.ShowDialog() == DialogResult.OK)
+                {
+                    ExecCmd(tmp + make, tmp + bo2 + " " + sv.FileName);
+                }
+                MessageBox.Show(String.Format("EBOOT Has Ben Successfully Created\nFile Size {0}\nFile Location {1}\nFile Extension {2}", FormatBytes(File.ReadAllBytes(sv.FileName).Length, 1, true), sv.FileName, Path.GetExtension(sv.FileName)));
+                File.Delete(tmp + bo2);
+                LoadFiles(tmp + bo2, Properties.Resources.bo2);
+            }
+            catch { }
+        }
+        private string FormatBytes(long bytes, int decimalPlaces, bool showByteType)
+        {
+            double newBytes = bytes;
+            string formatString = "{0";
+            string byteType = " B";
+
+            if (newBytes > 1024 && newBytes < 1048576)
+            {
+                newBytes /= 1024;
+                byteType = "KB";
+            }
+            else if (newBytes > 1048576 && newBytes < 1073741824)
+            {
+                newBytes /= 1048576;
+                byteType = " MB";
+            }
+            else
+            {
+                newBytes /= 1073741824;
+                byteType = " GB";
+            }
+            if (decimalPlaces > 0)
+                formatString += ":0.";
+            for (int i = 0; i < decimalPlaces; i++)
+                formatString += "0";
+            formatString += "}";
+            if (showByteType)
+                formatString += byteType;
+
+            return string.Format(formatString, newBytes);
+        }
+    }
+}
